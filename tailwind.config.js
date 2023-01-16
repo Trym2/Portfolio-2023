259b71282551a8437804ab3fae2b429145d5c1b5
Trym <trym-foss@hotmail.com>
2023-01-16 18:00:19 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': '"Open Sans", montserrat',
+      },
+      fontSize: {
+        '3xl': '32px',
+      }
+    },
+  },
+  plugins: [],
+}
