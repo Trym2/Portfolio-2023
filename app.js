7c0298d0fb510aacf524b4f6e69629c4b454787f
Trym <trym-foss@hotmail.com>
2023-02-09 19:25:36 +0100

    updates

---
+++ app.js
@@ -0,0 +1,18 @@
+const body = document.getElementById("body")
+
+const fadeIn = document.getElementsByClassName("fade-in")
+const fadeUp = document.getElementsByClassName("fade-up")
+
+console.log(body)
+
+
+document.addEventListener("scroll", () => {
+    for (let i = 0; i < fadeIn.length; i++) {
+        const mathy = fadeIn[i].getBoundingClientRect().top - window.innerHeight
+        if (mathy < 0) {
+            fadeIn[i].classList.add('in-view');
+        }
+    }
+
+  
+})
