189240b9028bcc1ae593eea5c2b95630d3016dac
Trym <trym-foss@hotmail.com>
2023-03-08 23:49:12 +0100

    added scroll animation

--- app.js
+++ app.js
@@ -5,14 +5,6 @@ const fadeUp = document.getElementsByClassName("fade-up")
 
 console.log(body)
 
-
-for (let i = 0; i < fadeIn.length; i++) {
-    const mathy = fadeIn[i].getBoundingClientRect().top - window.innerHeight
-    if (mathy < 0) {
-        fadeIn[i].classList.add('in-view');
-    }
-}
-
 document.addEventListener("scroll", () => {
 
     for (let i = 0; i < fadeIn.length; i++) {
